cb2be38ae2b389c3fe830a40a2acc0cbea84cdcc
Sourav Maity <sourav.maity2@mindtree.com>
2022-09-06 14:35:18 +0530

    Stable release : Module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import QuizQuestion from "./QuizQuestion.js";
+import QuizEnd from "./QuizEnd.js"
 
 let quizData = require('./quiz_data.json');
 
@@ -14,8 +15,14 @@ class Quiz extends Component {
     }
 
     render() {
+        const isQuizEnd = quizData.quiz_questions.length === (this.state.quiz_position - 1)
         return (
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>
+            <div>
+                {isQuizEnd ? 
+                <QuizEnd /> :
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>
+                }
+            </div>
         )
         
     }
