69a4c79b6bcaadf95cc34b68161d2e53577d0597
Sourav Maity <sourav.maity2@mindtree.com>
2022-09-07 11:53:49 +0530

    Stable Release : Module5

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,13 +14,18 @@ class Quiz extends Component {
         }
     }
 
+    showNextQuestion() {
+        console.log('next question handler clicked!')
+        this.setState(currentState => ({ quiz_position : currentState.quiz_position + 1 }))
+    }
+
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
             <div>
                 {isQuizEnd ? 
                 <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>
+                <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>
                 }
             </div>
         )
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 
 class QuizQuestionButton extends Component {
 
+    handleClick() {
+        console.log('handler clicked')
+        this.props.clickHandler(this.props.button_text)
+    }
+
     render() {
         return (
             <li>
-                <button>{this.props.button_text}</button>
+                <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
             </li>
         )
     }
